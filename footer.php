7a75cac8423c6e767430030b7508859b969752fe
Hung Pham <phamphihungbk@gmail.com>
2018-12-25 01:59:40 +0800

    add new header and footer

---
+++ footer.php
@@ -0,0 +1,10 @@
+    </div><!-- #content -->
+
+    <footer class="site-footer">
+    </footer>
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body>
+<div id="page" class="site">
+
+    <header>
+
+    </header>
+
+<div id="content" class="site-content">
